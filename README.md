06e927c4695a1d99cc3ef905969adad531197373
Hayden Rouille <hayden@rouille.net>
2024-01-06 11:00:05 -0500

    add floating window

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # bomberman.nvim
 
 # Development
-## lazy.nvim
+## using lazy.nvim
 - Add `{ dir = "~/projects/bomberman.vim" }` to your lazy.nvim configuration (or wherever the folder lives)
+
+# TODO:
--- lua/bomberman/fns.lua
+++ lua/bomberman/fns.lua
@@ -1,6 +1,11 @@
 local M = {}
 
 function M.load()
+    vim.lsp.util.open_floating_preview({
+        title = "Hello World!",
+        body = "This is an example floating window"
+    }, "center", { border = "single", width = 100, height = 100 })
+
     print("Loaded")
 end
 
